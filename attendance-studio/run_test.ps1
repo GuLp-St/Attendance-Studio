ca810e8193e571a7895319f9498ec8ebc3d97972
GuLp-St <kiusiangyu3118@gmail.com>
2026-03-31 22:15:51 +0800

    chore: remove leftover test/debug scripts from attendance-studio root

--- attendance-studio/run_test.ps1
+++
@@ -1,6 +0,0 @@
-$job = Start-Job { Set-Location C:\Users\Administrator\Desktop\Attendance-Studio\attendance-studio; npm run dev }
-Start-Sleep -Seconds 5
-npm install puppeteer
-node test_error.js
-Stop-Job $job
-Remove-Job $job
